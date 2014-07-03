c96911151613dafc61aeebeec0a73df19816a0bd
rednatto <rednatto@gmail.com>
2014-07-03 12:56:03 +0200

    stack.h
    asdf

---
+++ stack.h
@@ -0,0 +1,32 @@
+
+#ifndef __STACK
+#define __STACK
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+
+struct _stack 
+{
+	
+	void *val;
+	struct _stack *next;
+
+};
+
+typedef struct _stack *STACK; 
+
+
+typedef void (*memhandler)(void *);
+#define STNULL (STACK)NULL
+
+
+STACK STACKpop( STACK s, memhandler mhf );
+STACK STACKpush( STACK s, void *val );
+void *STACKtop( STACK s );
+STACK STACKempty( void );
+bool STACKisEmpty( STACK s );
+void STACKdestroy( STACK s, memhandler mhf );
+
+#endif
